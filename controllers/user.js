374366e98aa113387fe1d5caa250a94969c83a07
Santosh Suryawanshi <santosh_suryawanshi@amicusglobal.com>
2018-06-08 20:18:10 +0530

    validation

---
+++ controllers/user.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    signUp : async (req,res)=>{ 
+        console.log('user Controller signUp')
+    },
+    signIn : async (req,res)=>{ 
+        console.log('user Controller signIn')
+    },
+    secret : async (req,res)=>{ 
+        console.log('user Controller secret')
+    }
+}
---
+++ routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = require('express-promise-router')();
+
+const userController = require('../controllers/user');
+const { validateBody ,schemas } = require('../helpers/routerHelper');
+
+router.route('/signup').post(validateBody(schemas.authSchema), userController.signUp);
+router.route('/signin').post(userController.signIn);
+router.route('/secret').get(userController.secret);
+
+module.exports = router;
