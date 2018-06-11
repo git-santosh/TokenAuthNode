2f8946285954f4a98ba6473320bade639c36b13e
santosh suryawanshi <santosh.suryawanshi28@gmail.com>
2018-06-11 11:18:11 +0530

    mongoose user create

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,15 @@
 
+const User = require('../models/users');
 module.exports = {
     signUp : async (req,res)=>{ 
-        console.log('user Controller signUp')
+        console.log('user Controller signUp');
+        const { email , password } = req.value.body;
+
+        let newUser = new User({email,password});
+
+        await newUser.save();
+
+        return res.status(200).json({'resut':'user created'})
     },
     signIn : async (req,res)=>{ 
         console.log('user Controller signIn')
